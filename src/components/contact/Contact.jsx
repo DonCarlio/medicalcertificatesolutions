f728a32ee159c429d71d389945d8544b260aeb40
DonCarlio <doncarlio770@gmail.com>
2023-01-11 01:53:35 +0100

    Delete duplicate

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -51,7 +51,6 @@ const Contact = () => {
           <form ref={form} onSubmit={sendEmail}>
             <input type="text" name='name' placeholder='Your Full Name' required />
             <input type="email" name='email' placeholder='Your Email Address' required />
-            <input type="email" name='email' placeholder='Your Message' required />
             <textarea name="message" id="message" rows="7" placeholder='Your Message' required></textarea>
             <button type='submit' className='btn btn-primary'>Send Message</button>
           </form>
